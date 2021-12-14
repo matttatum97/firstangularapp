e4475d7dc0af27f1bc21c439b2240f6f0ed9d485
matttatum97 <matttatum97@gmail.com>
2021-12-14 15:44:20 -0500

    Added hover wiggle effect

--- src/app/meme/meme.component.ts
+++ src/app/meme/meme.component.ts
@@ -10,14 +10,30 @@ import { MemeService } from '../meme.service';
 export class MemeComponent implements OnInit {
 
   memes: Meme[] = [];
+  savedMemes: Meme[] = [];
   
   constructor(private memeService: MemeService) { }
 
   ngOnInit(): void {
     this.memeService.getMemes().subscribe(payload => {
       this.memes = payload.data.memes;
-      console.log("PAYLOAD",payload.data.memes)
+      console.log("PAYLOAD:",payload.data.memes)
     })
+  }
+
+  onShow(event: any){
+    // alert(event.target.value)
+    let imgId = event.target.id;
+    console.log(imgId);
+   
+    for(let i = 0; i < this.memes.length; i++) {
+      if (this.memes[i] === imgId) {
+        this.savedMemes.push(this.memes[i]) 
+      }
+    } 
+    console.log("SAVED MEMES",this.savedMemes);
+  
+   
   }
 
 }
