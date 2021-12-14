1dea08e281be117bbfa716eee30c7e0fbbf70804
matttatum97 <matttatum97@gmail.com>
2021-12-14 16:52:52 -0500

    Add a buffer page between memes and starting page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MemeComponent } from './meme/meme.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: "meme", component: MemeComponent},
-  {path: "", redirectTo: "meme", pathMatch: 'full'}
+  {path: "meme", component: MemeComponent}
 ];
 
 @NgModule({
